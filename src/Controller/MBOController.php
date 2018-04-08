6329d812225d561e4f8a5c1e40669ab30a6fb629
yakoub <yakoub@fedora.lan>
2018-04-08 16:18:53 +0300

    by year with nav menu

--- src/Controller/MBOController.php
+++ src/Controller/MBOController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
-use App\Entity\Person;
-use App\Form\PersonType;
-use App\Repository\PersonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+
+use App\Entity\Person;
+use App\Repository\PersonRepository;
+use App\Repository\MBOYearlyRepository;
+use App\Form\YearType;
 
 class MBOController extends Controller
 {
@@ -21,5 +23,22 @@ class MBOController extends Controller
     {
         $people = $personRepository->getRoot();
         return $this->render('mbo/tree.html.twig', ['people' => $people, 'head' => false]);
+    }
+
+    public function by_year(MBOYearlyRepository $mbo_repository, Request $request): Response
+    {
+        $year = 2018;
+        $form = $this->createForm(YearType::class, ['year' => $year]);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() and $form->isValid()) {
+            $year = $form->get('year')->getData();
+        }
+        $mbo_yearlies = $mbo_repository->findBy(['year' => $year]);
+        $context = array(
+            'form' => $form->createView(),
+            'year' => 2018,
+            'mbo_yearlies' => $mbo_yearlies,
+        );
+        return $this->render('mbo/mbo_browse.html.twig', $context);
     }
 }
