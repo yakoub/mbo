626b8aa32026dd4c07eb5f5ebb76f1ade07c8bc6
yakoub <yakoub@fedora.lan>
2018-04-14 21:44:34 +0300

    out

--- src/Repository/ObjectiveManagementRepository.php
+++ src/Repository/ObjectiveManagementRepository.php
@@ -18,33 +18,4 @@ class ObjectiveManagementRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ObjectiveManagement::class);
     }
-
-//    /**
-//     * @return ObjectiveManagement[] Returns an array of ObjectiveManagement objects
-//     */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('o')
-            ->andWhere('o.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('o.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?ObjectiveManagement
-    {
-        return $this->createQueryBuilder('o')
-            ->andWhere('o.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
