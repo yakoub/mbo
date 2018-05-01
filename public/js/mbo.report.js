c926b6bb32da9726544cabc6edcd23aecd352720
yakoub <yakoub@fedora.lan>
2018-05-01 18:05:33 +0300

    table total

--- public/js/mbo.report.js
+++ public/js/mbo.report.js
@@ -28,6 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
 mbo_report.scan = function() {
     this.partitions = this.table.tBodies;
     var total_score = 0;
+    var table_total = 0;
     for (var iter = 0; iter < this.partitions.length; iter++) {
         var partition = this.partitions[iter];
         if (partition.classList.contains('partition')) {
@@ -46,6 +47,7 @@ mbo_report.scan = function() {
             partition.rows[0].cells[this.hw].textContent = weight_sum.toFixed(2);
             partition.rows[0].cells[this.hs].textContent = score_sum.toFixed(2);
             total_score += score_sum;
+            table_total += (score_sum ? score_sum : weight_sum);
         }
         else {
           var achieve = partition.rows[0].cells[this.fa].children[0].value;
@@ -54,8 +56,10 @@ mbo_report.scan = function() {
           partition.rows[0].cells[this.fs].textContent = score.toFixed(2);
           total_score += score;
           partition.rows[0].cells[this.ft].textContent = total_score.toFixed(2);
+          table_total += score;
         }
     }
+    document.querySelector('var.table-total').textContent = table_total.toFixed(2);
 };
 
 mbo_report.update = function(row) {
@@ -83,13 +87,17 @@ mbo_report.update_final = function(row, partition) {
   score_total += score_diff;
   row.cells[this.ft].textContent = score_total.toFixed(2);
 
-  if (partition.classList.contains('ceo')) {
-    var vp = partition.parentNode.querySelector('tbody.vp');
+  var table_total = score_total;
+
+  if (partition.classList.contains('vp')) {
+    var vp = partition.parentNode.querySelector('tbody.ceo');
     score_total = vp.rows[0].cells[this.ft].textContent;
     score_total = score_total.length == 0 ? 0 : parseFloat(score_total);
     score_total += score_diff;
     vp.rows[0].cells[this.ft].textContent = score_total.toFixed(2);
+    table_total = score_total;
   }
+  document.querySelector('var.table-total').textContent = table_total.toFixed(2);
 };
 
 mbo_report.update_partition = function(row, partition) {
@@ -98,7 +106,7 @@ mbo_report.update_partition = function(row, partition) {
   var achieve = row.cells[this.a].children[0].value;
   achieve = achieve.length == 0 ? 0 : parseFloat(achieve);
 
-  this.update_weight_sum(partition);
+  var weight_diff = this.update_weight_sum(partition);
 
   var score = weight * (achieve/100);
   
@@ -106,8 +114,16 @@ mbo_report.update_partition = function(row, partition) {
   row.cells[6].textContent = score.toFixed(2);
   score_diff = score_diff.length == 0 ? 0 : parseFloat(score_diff);
   score_diff = score - score_diff;
-  this.update_total_score(partition, score_diff);
-}
+  var score_sum = this.update_total_score(partition, score_diff);
+
+  if (score_diff != 0 && score_sum != 0) {
+    this.update_total_table(_diff);
+    return;
+  }
+  if (weight_diff != 0 && score == 0) {
+    this.update_total_table(weight_diff);
+  }
+};
 
 mbo_report.update_total_score = function(partition, score_diff) {
   var score_sum = partition.rows[0].cells[this.hs].textContent;
@@ -125,7 +141,16 @@ mbo_report.update_total_score = function(partition, score_diff) {
     score_total += total_diff;
     partition.parentNode.tBodies[iter].rows[0].cells[this.ft].textContent = score_total.toFixed(2);
   }, this);
-}
+
+  return score_sum;
+};
+
+mbo_report.update_total_table = function (total_diff) {
+  var table_total = document.querySelector('var.table-total').textContent;
+  table_total = table_total.length == 0 ? 0 : parseFloat(table_total);
+  table_total += total_diff;
+  document.querySelector('var.table-total').textContent = table_total.toFixed(2);
+};
 
 mbo_report.update_weight_sum = function (partition) {
   if (!partition.classList.contains('partition')) {
@@ -138,6 +163,10 @@ mbo_report.update_weight_sum = function (partition) {
       weight = weight.length == 0 ? 0 : parseFloat(weight);
       weight_sum += weight;
   }
+  var weight_diff = partition.rows[0].cells[2].textContent;
   partition.rows[0].cells[2].textContent = weight_sum.toFixed(2);
+  weight_diff = weight_diff.length == 0 ? 0 : parseFloat(weight_diff);
+  weight_diff = weight_sum - weight_diff;
+  return weight_diff;
 };
 
