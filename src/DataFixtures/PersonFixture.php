cc57844b286e1b49b04c63c7753c0ea8dc215ba4
yakoub abaya <sting606@gmail.com>
2021-05-01 23:53:35 +0300

    passwords fixtures

--- src/DataFixtures/PersonFixture.php
+++ src/DataFixtures/PersonFixture.php
@@ -3,16 +3,27 @@
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 use App\Entity\Person;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class PersonFixture extends Fixture
 {
+    private $pass_encoder;
+
+    public function __construct(UserPasswordEncoderInterface $pass_encoder) {
+      $this->pass_encoder = $pass_encoder;
+    }
     public function load(ObjectManager $manager)
     {
         foreach ([1, 2] as $f) {
           $first = new Person();
           $first->setName('manager-' . $f);
+          $first->setPassword($this->pass_encoder->encodePassword(
+            $first,
+            'pass-' . $f,
+          ));
+
           $first->setFullName('manager-' . $f);
           $first->setEmail($first->getName() . '@dd.d');
           $first->setActive(true);
@@ -21,6 +32,10 @@ class PersonFixture extends Fixture
           foreach (range(1, 10) as $s) {
             $second = new Person();
             $second->setName("manager-$f-$s");
+            $second->setPassword($this->pass_encoder->encodePassword(
+              $second,
+              "pass-$f-$s",
+            ));
             $second->setFullName("manager-$f-$s");
             $second->setEmail($second->getName() . '@dd.d');
             $second->setManager($first);
@@ -31,6 +46,10 @@ class PersonFixture extends Fixture
             foreach (range(1, 5) as $t) {
               $third = new Person();
               $third->setName("employee-$f-$s-$t");
+              $third->setPassword($this->pass_encoder->encodePassword(
+                $third,
+                "pass-$f-$s-$t",
+              ));
               $third->setFullName("employee-$f-$s-$t");
               $third->setEmail($third->getName() . '@dd.d');
               $third->setManager($second);
